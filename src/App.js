6d64c79ed1a30b88c3c1aba0005cb0dfcd4a0eb5
f-seif <f.seif@yandex.com>
2022-07-23 16:23:22 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+   fullName: "Sara",
+   bio: "Sara's bio",
+   imgSrc: "./profilePhoto.jpg",
+   profession: "teacher",
+   show: false,
+   cnt: 0
+ };
+ toggleShow=()=>this.setState({show: !this.state.show})
+ componentDidMount() {
+    setInterval(() => {
+      this.setState((prevState) => ({
+        cnt: prevState.cnt + 1,
+      }));
+    },1000);
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p></p>
+          <button onClick={this.toggleShow}>Toggle profile</button>
+          {this.state.show && (
+            <div style={{width:"240px", background:"darkcyan", marginTop:"16px"}}>
+              <p>{this.state.fullName}</p>
+              <p>{this.state.bio}</p>
+              <img src={this.state.imgSrc} style={{width:"100%"}} alt="profilePhoto"></img>
+              <p>{this.state.profession}</p>
+              <div> Count: {this.state.cnt} sec</div>
+            </div>
+          )}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
